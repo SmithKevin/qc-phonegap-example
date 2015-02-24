87ffc11572ae77822d52bbb3db81fd3591e495c5
Kevin Smith <kevin_smith@quantcast.com>
2015-02-24 15:30:34 -0800

    added additional feature examples

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -31,11 +31,37 @@ var app = {
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+    // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-    	      QuantcastMeasurement.setUpQuantcastMeasurement('0zc5v23gnvnbid2y-hmmzpb10u0ejwb4p', 'User label', ['abel1', 'label2'] );
+        QuantcastMeasurement.setDebugLogging(true);
+        QuantcastMeasurement.uploadEventCount(100);
+        QuantcastMeasurement.setUpQuantcastMeasurement('0zc5v23gnvnbid2y-hmmzpb10u0ejwb4p', 'User label', ['abel1', 'label2'] );
+        
         app.receivedEvent('deviceready');
     },
+    
+    onDeviceReadyManual:function() {
+        document.addEventListener('pause', app.onPause, false);
+        document.addEventListener('resume', app.onResume, false);
+        
+        QuantcastMeasurement.setDebugLogging(true);
+        QuantcastMeasurement.setGeoLocation(true);
+        QuantcastMeasurement.uploadEventCount(100);
+        QuantcastMeasurement.beginMeasurementSession(app.hashcallback, '1baal30usbrl0ed2-tjb0tbwca3tb5s4r', 'User label', 5 )
+        
+        app.receivedEvent('deviceready');
+        
+    },
+    
+    
+    onPause: function() {
+        QuantcastMeasurement.pauseMeasurementSession(['label1', 'label2']);
+    },
+        
+    onResume: function() {
+        QuantcastMeasurement.resumeMeasurementSession(['label1', 'label2']);
+    },
+    
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
@@ -46,5 +72,30 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    },
+    
+    hashcallback: function(hash){
+        alert("SUCCESS: \r\n"+hash );
+    },
+        
+    optOutCallback: function(optedOut){
+        alert("IS Opted out: \r\n"+optedOut );
+    },
+        
+        
+    logEvent: function(){
+        QuantcastMeasurement.logEvent('ButtonPress', ['label1', 'label2']);
+    },
+        
+    changeUser: function(){
+        QuantcastMeasurement.recordUserIdentifier(app.hashcallback, 22, 5);
+    },
+        
+    displayOptOut: function(){
+        QuantcastMeasurement.displayUserPrivacyDialog(app.optOutCallback);
+    },
+        
+    goGoogle: function(){
+        var ref = window.open('http://google.com', '_blank', 'location=yes');
     }
 };
